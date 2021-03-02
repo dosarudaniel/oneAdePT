b173ab9fcccda6d383ed4dcb18dc2f5898909c7a
dosarudaniel <dosarudaniel@gmail.com>
2021-03-02 11:50:11 +0100

    uncommented code

--- test9.cpp
+++ test9.cpp
@@ -153,7 +153,7 @@ int main(void)
 
       cgh.parallel_for(sycl::nd_range<3>(nblocks * nthreads, nthreads), [=](sycl::nd_item<3> item_ct1) {
         adept::BlockData<MyTrack> *block_ct0 = (adept::BlockData<MyTrack> *)(&block_acc_ct0[0] + block_offset_ct0);
-        //testTrackBlock(block_ct0, item_ct1);
+        testTrackBlock(block_ct0, item_ct1);
       });
     });
   } ///< note that we are passing a host block type allocated on device
