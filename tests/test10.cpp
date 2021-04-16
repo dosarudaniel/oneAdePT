624498ef47796b8497f3b78ac78fb8ef2c8800aa
dosarudaniel <dosarudaniel@gmail.com>
2021-04-16 12:27:13 +0200

    Tried sqrt, worked; log does not

--- tests/test10.cpp
+++ tests/test10.cpp
@@ -1,28 +1,35 @@
 // SPDX-FileCopyrightText: 2020 CERN
 // SPDX-License-Identifier: Apache-2.0
 
+/* Use this command to avoid cmake: 
+
+/home/dadosaru/sycl_workspace/llvm/build//bin/clang++ ../../tests/test10.cpp -o test10 -lsycl -fsycl -fsycl-targets=nvptx64-nvidia-cuda-sycldevice -fsycl-unnamed-lambda -Wno-unknown-cuda-version -D_ONEADEPT_ -DROOT_NO_INT128 -DG4HepEm_CUDA_BUILD --verbose
+*/
+
+
 #include <CL/sycl.hpp>
-//#include <dpct/dpct.hpp>
 
 #include <iostream>
 #include <stdlib.h>
+#include <math.h>
 
 void kernel(double *d)
 {
-  *d = std::log(*d);
+  *d = sqrt(*d); // passes this point
+
+  *d = log(*d); // fails here
 }
+
 
 int main(void)
 {
   sycl::default_selector device_selector;
 
   sycl::queue q_ct1(device_selector);
-  std::cout <<  "Running on "
-	    << q_ct1.get_device().get_info<cl::sycl::info::device::name>()
-	    << "\n";
+  std::cout <<  "Running on " << q_ct1.get_device().get_info<cl::sycl::info::device::name>() << "\n";
  
-
-  double *d_dev_ptr;
+  double value = 10;
+  double *d_dev_ptr = &value;
 
   d_dev_ptr  = sycl::malloc_device<double>(1, q_ct1);
 
@@ -41,7 +48,7 @@ int main(void)
 
   q_ct1.wait_and_throw();
 
-  double d = std::log(10);
+  double d = std::log(value);
   
   std::cout << std::endl;
   std::cout << "double:" << std::endl;
